3ae2c0aee0b8420b21844660d722e0afb6a74d88
PedroLeocir <110078615+Pedro-Leocir-Angelin-Zwirtes@users.noreply.github.com>
2023-10-19 23:47:42 -0300

    Create funcoes.js
    Em nosso arquivo funções criamos várias funções e aprendemos a exportar todas elas de uma unica vez

---
+++ modulos/funcoes.js
@@ -0,0 +1,17 @@
+function multi(a,b){
+    return a * b;
+}
+
+function subt(a,b) {
+    return a - b;
+}
+
+function div(a,b) {
+    return a / b;
+}
+
+module.exports = {
+    multi,
+    subt,
+    div
+}
